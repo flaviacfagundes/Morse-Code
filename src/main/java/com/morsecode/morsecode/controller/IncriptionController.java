f871704318732ee434b5f1ddade5f478cb51b8ba
Lucas Aioria Serpa <136469417+LucasAioriaSerpa@users.noreply.github.com>
2025-10-23 17:18:42 -0300

    incription view done frontend

---
+++ src/main/java/com/morsecode/morsecode/controller/IncriptionController.java
@@ -0,0 +1,15 @@
+package com.morsecode.morsecode.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+
+public class IncriptionController {
+    public TextArea inputTextArea;
+    public TextArea outputTextArea;
+
+    public void handleEncrypt(ActionEvent actionEvent) {
+    }
+
+    public void handleClear(ActionEvent actionEvent) {
+    }
+}
